350ee9ac2181459715f985f702c8ef79d9ec7b77
Miguel Henrique <miguelhp373@gmail.com>
2021-02-15 11:19:24 -0300

    update organization files and corretions actions

---
+++ src/components/home/ButtonSubmit.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#submit-email-user').click(function(){
+        if($('#email-input').val().length > 0){
+            console.log('ok')
+        }else{
+            $('#email-input').focus();
+            $('#alert-msg-form').removeClass('hidden');//adiciona a classe que oculta o alerta de erro
+            $('#lb-email-input').css({ 'margin-right': ' 590px', 'margin-top': '23px', 'font-size': '13px' });
+            $('#email-input').css('border-bottom', '2.5px solid #ffa00a');
+        }
+    })
+});
