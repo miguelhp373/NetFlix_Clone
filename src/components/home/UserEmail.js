0d54c9b763d14c0406b25519e8ece61d78814745
Miguel Henrique <miguelhp373@gmail.com>
2021-02-16 13:27:35 -0300

    Home Complete

--- src/components/home/UserEmail.js
+++ src/components/home/UserEmail.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+//input top
     $('#email-input').focus(function(){
     $('#email-input').css('border', '1px solid #0071eb');
       $('#lb-email-input').css({ 'margin-right': ' 590px', 'margin-top': '23px', 'font-size': '13px' }); 
@@ -18,5 +20,28 @@ $(document).ready(function () {
             $('#email-input').css('border-bottom', '2.5px solid #ffa00a');
         }
     })
+
+
+//input-footer
+
+    $('#email-input-02').focus(function(){
+        $('#email-input-02').css('border', '1px solid #0071eb');
+          $('#lb-email-input-02').css({ 'margin-right': ' 590px', 'margin-top': '10px', 'font-size': '13px' }); 
+        })
+    
+        $('#email-input-02').focusout(function(){
+            $('#email-input-02').css('border', 'none');
+            if($('#email-input-02').val().length > 0){
+                console.log('email ok')
+                $('#alert-msg-form-02').addClass('hidden');//adiciona a classe que oculta o alerta de erro
+                $('#lb-email-input-02').css({ 'margin-right': ' 590px', 'margin-top': '10px', 'font-size': '13px' });
+    
+            }else{
+                console.log('email required')
+                $('#alert-msg-form-02').removeClass('hidden');//adiciona a classe que oculta o alerta de erro
+                $('#lb-email-input-02').css({ 'margin-right': ' 590px', 'margin-top': '26px' });
+                $('#email-input-02').css('border-bottom', '2.5px solid #ffa00a');
+            }
+        })
 
 })
