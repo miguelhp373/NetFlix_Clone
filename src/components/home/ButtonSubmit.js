41638afb804d1a8ca98b7c4dce056d8cc91ded39
Miguel Henrique <miguelhp373@gmail.com>
2021-02-20 21:07:50 -0300

    Create Section Forms and Inputs

--- src/components/home/ButtonSubmit.js
+++ src/components/home/ButtonSubmit.js
@@ -9,4 +9,16 @@ $(document).ready(function () {
             $('#email-input').css('border-bottom', '2.5px solid #ffa00a');
         }
     })
+
+
+    $('#submit-email-help').click(function(){
+        if($('#email-input-02').val().length > 0){
+            console.log('ok')//redireciona
+        }else{
+            $('#email-input-02').focus();
+            $('#alert-msg-form-02').removeClass('hidden');//adiciona a classe que oculta o alerta de erro
+            $('#lb-email-input-02').css({ 'margin-right': ' 590px', 'margin-top': '23px', 'font-size': '13px' });
+            $('#email-input-02').css('border-bottom', '2.5px solid #ffa00a');
+        }
+    })
 });
