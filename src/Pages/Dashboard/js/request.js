938c4da1e1cd33badda193c82a9a52c9744e90da
Miguel Henrique <miguelhp373@gmail.com>
2021-07-10 23:02:17 -0300

    version 1.0 - landingPage complete :+1:

--- src/Pages/Dashboard/js/request.js
+++ src/Pages/Dashboard/js/request.js
@@ -1,4 +1,5 @@
-//https://api.themoviedb.org/3/discover/movie?api_key=51bdd827768a48612af8d3a0bd074a01
+$(document).ready(function () {
+    //https://api.themoviedb.org/3/discover/movie?api_key=51bdd827768a48612af8d3a0bd074a01
 
 const ApiKey = '?api_key=51bdd827768a48612af8d3a0bd074a01'
 let baseURL = 'https://api.themoviedb.org/3/'
@@ -15,7 +16,27 @@ $(document).ready(function () {
         }   
 })
     .then(function (data) {
-        console.log(data)
+//data.results.length
+            for(let i = 2; i < 20 ; i++){
+                console.log(data.results[i])
+                console.log(data.results[i].poster_path)//retorna o array separado
+                //var poster = document.getElementById('row_')
+                rowDiscover = document.getElementById('row02_movies_popular')
+                
+                var posterimg = 'http://image.tmdb.org/t/p/w300' + data.results[i].poster_path
+                 console.log(posterimg) 
+                rowDiscover.innerHTML +=  `
+                <li>
+                <a href="#">
+                  <img src="${posterimg}" id="item0${i}" alt="">
+                </a>
+              </li>
+                `
+                
+            }
+
+
     })
 
 })
+});
